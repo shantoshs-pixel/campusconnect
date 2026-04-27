be63fcc2aef6c4204ef51f68b4bd88d9298f0ab0
shantosh <shantoshoff@gmail.com>
2026-04-28 02:54:46 +0530

    fix signup route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/register', register);
+router.post('/signup', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
